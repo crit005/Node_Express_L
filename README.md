1bb4a353d9ae79b28cf73c811b61506af7f9f571
Chhoun Sokonthea <sokonthea.chhoun@yahoo.com>
2020-02-05 18:49:31 +0700

    get start with mongoose

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Mongoose and Model Setup
+
+## Mongo Command
+```
+show dbs // Show all databases
+use db_name // create database and switch to database
+db.createCollection('Collection_name') // Create Collection
+db.collection_name.insert({title:'your title', author: 'author name', body:'your body'}) // Insert to Collection
+db.collection_name.find().pretty() // Read and search in collection
+```
--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongose = require('mongoose');
+
+mongose.connect('mongodb://localhost/nodekb');
+
+let db = mongose.connection;
+
+//* Check Connection
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+//* Check for DB error
+db.on('error', (err) => {
+    console.log(err);
+});
 
 //* Init App
 const app = express();
+
+//* Bring in Models
+let Article = require('./models/articles');
 
 //* Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,32 +28,16 @@ app.set('view engine', 'pug');
 
 //* Home rout
 app.get('/', (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Konthea',
-            body: 'This is article one'
-        },
-        {
-            id: 2,
-            title: 'Article Two',
-            author: 'Konthea',
-            body: 'This is article two'
-        },
-        {
-            id: 3,
-            title: 'Article three',
-            author: 'Konthea',
-            body: 'This is article three'
-        },
-
-    ];
-    res.render('index', {
-        title: 'Nodejs Express/Home',
-        articles: articles
-
-    });
+    Article.find({}, (err, articles) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Nodejs Express/Home',
+                articles: articles
+            });
+        }
+    })
 });
 
 //* Start Server
